c1ab8930feff5d488246dac05d1af20834fd2d52
Tomasz Brazkiewicz <tom.brazkiewicz@gmail.com>
2017-08-23 09:05:08 +0200

    First Commit

---
+++ Trening_Bookstore/src/main/java/com/tomkek/TreningBookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.tomkek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreningBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreningBookstoreApplication.class, args);
+	}
+}
